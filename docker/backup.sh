ec689336665abd125cd377f97f5a98eedc91680b
Sietse Snel <s.t.snel@uu.nl>
2025-07-04 11:45:07 +0200

    Docker setup backup/restore: add CKAN storage
    Add CKAN storage directory to backup / restore scripts, so that
    files in local CKAN storage (in particular the site logo) can
    also be restored from backup.

--- docker/backup.sh
+++ docker/backup.sh
@@ -10,4 +10,5 @@ then echo "No staging dir provided. Setting it to current working directory."
 fi
 
 docker exec ckan /bin/bash -c "cd /etc/ckan; tar cv default" | gzip -9 > "${STAGINGDIR}/ckan-settings.tar.gz"
+docker exec ckan /bin/bash -c "cd /; tar cv ckanstorage" | gzip -9 > "${STAGINGDIR}/ckan-storage.tar.gz"
 docker exec ckan /bin/bash -c "PGPASSWORD=\"\$POSTGRES_PASSWORD\" pg_dump -d ckan_default -h db -U ckan" | gzip -9 > "${STAGINGDIR}/ckan-db.sql.gz"
